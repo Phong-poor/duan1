5f7876ad7f2eac473b8b662c41fc6f83def34e03
Phongpoor <phongtqpk04300@gmail.com>
2025-11-11 15:20:23 +0700

    add backend

---
+++ backend/Database/config.php
@@ -0,0 +1,10 @@
+<?php 
+const HOST = 'localhost';
+const USERNAME = 'root';
+const DATABASE = 'duanmau';
+const PASSWORD = '';
+define('SITE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/php1/website');
+define('SITE_URL', '/php1/website');
+define('USERNAME_EMAIL', 'phongtqpk04300@gmail.com'); // thay bằng email của các bạn
+define('PASSWORD_EMAIL', 'fgbt hbwj owjs byjr'); // thay bằng password của các bạn
+?>
