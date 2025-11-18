468431ac987b20f0ef205b3ce18f80dddb130ebc
Phongpoor <phongtqpk04300@gmail.com>
2025-11-18 22:30:59 +0700

    Tạo header-admin

---
+++ frontend/src/Header-admin.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="d-flex align-items-center justify-content-between px-4 py-3 border-bottom">
+
+
+    <div class="flex-grow-1 text-center fw-semibold fs-5 welcome">
+      Xin chào, Phong
+    </div>
+
+    <div>
+      <button class="btn btn-primary me-2 back">Trở về trang web</button>
+      <button class="btn btn-danger">Đăng xuất</button>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.welcome{
+    color: white;
+}
+header{
+    background-color: rgb(85, 85, 85);
+}
+</style>
