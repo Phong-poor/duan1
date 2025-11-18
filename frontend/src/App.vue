84e6d700bbcc71f8a6f00be252da2cb13c50d1fb
Thành <thanhqdpk04326@gmail.com>
2025-11-18 23:36:44 +0700

    Update giao diện trang web

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Trangchu from './trangchu.vue';
+import thongTin from './ThongTin.vue';
+import chitietdonhang from './ChiTietDonHang.vue';
+import sanpham from './SanPham.vue';
+import chitiet from './ChiTiet.vue';
+</script>
+<template>
+
+  <main>
+    <!-- <Trangchu /> -->
+    <!-- <thongTin/> -->
+    <!-- <chitietdonhang/> -->
+     <!-- <sanpham/> -->
+      <chitiet/>
+  </main>
+  <FooterComponent  />
+</template>
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
