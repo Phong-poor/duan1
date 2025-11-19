f8aa1a8a730782a447c5e03b3cb28a52c7017f1d
Phongpoor <phongtqpk04300@gmail.com>
2025-11-19 14:58:19 +0700

    sửa router

--- frontend/src/router/Router.js
+++ frontend/src/router/Router.js
@@ -5,12 +5,14 @@ import Quanlydanhmuc from '../components/Admin/Quanlydanhmuc.vue';
 import Quanlythuonghieu from '../components/Admin/Quanlythuonghieu.vue';
 import Quanlykhachhang from '../components/Admin/Quanlykhachhang.vue';
 import Quanlydonhang from '../components/Admin/Quanlydonhang.vue';
+import Dashboard from '../components/Admin/Dashboard.vue';
 const routes = [
     { path: '/', name: 'Quanlysanpham', component: Quanlysanpham, meta: { isAuth: false } },
     { path: '/Quanlydanhmuc', name: 'Quanlydanhmuc', component: Quanlydanhmuc, meta: { isAuth: false } },
     { path: '/Quanlythuonghieu', name: 'Quanlythuonghieu', component: Quanlythuonghieu, meta: { isAuth: false } },
     { path: '/Quanlykhachhang', name: 'Quanlykhachhang', component: Quanlykhachhang, meta: { isAuth: false } },
     { path: '/Quanlydonhang', name: 'Quanlydonhang', component: Quanlydonhang, meta: { isAuth: false } },
+    { path: '/Dashboard', name: 'Dashboard', component: Dashboard, meta: { isAuth: false } },
 ]
 
 const router = createRouter({
