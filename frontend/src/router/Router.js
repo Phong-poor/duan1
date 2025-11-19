461bdc0f0b4c3f7f3df32fddb1207439f4be4fac
Phongpoor <phongtqpk04300@gmail.com>
2025-11-19 12:26:02 +0700

    cập nhật router

--- frontend/src/router/Router.js
+++ frontend/src/router/Router.js
@@ -2,9 +2,15 @@ import {createWebHistory, createRouter} from 'vue-router'
 
 import Quanlysanpham from '../components/Admin/Quanlysanpham.vue';
 import Quanlydanhmuc from '../components/Admin/Quanlydanhmuc.vue';
+import Quanlythuonghieu from '../components/Admin/Quanlythuonghieu.vue';
+import Quanlykhachhang from '../components/Admin/Quanlykhachhang.vue';
+import Quanlydonhang from '../components/Admin/Quanlydonhang.vue';
 const routes = [
     { path: '/', name: 'Quanlysanpham', component: Quanlysanpham, meta: { isAuth: false } },
     { path: '/Quanlydanhmuc', name: 'Quanlydanhmuc', component: Quanlydanhmuc, meta: { isAuth: false } },
+    { path: '/Quanlythuonghieu', name: 'Quanlythuonghieu', component: Quanlythuonghieu, meta: { isAuth: false } },
+    { path: '/Quanlykhachhang', name: 'Quanlykhachhang', component: Quanlykhachhang, meta: { isAuth: false } },
+    { path: '/Quanlydonhang', name: 'Quanlydonhang', component: Quanlydonhang, meta: { isAuth: false } },
 ]
 
 const router = createRouter({
